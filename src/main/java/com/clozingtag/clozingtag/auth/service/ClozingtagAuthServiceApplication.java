f295e03103c3eaf515e4833ec882448f11edb0fc
Saint Deemene <deemene.saint@loblaw.ca>
2025-03-04 12:11:54 +0000

    Authorization service setup

---
+++ src/main/java/com/clozingtag/clozingtag/auth/service/ClozingtagAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.auth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClozingtagAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClozingtagAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clozingtag/clozingtag/auth/service/ClozingtagAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClozingtagAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
